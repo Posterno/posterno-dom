6ef45a0d17de380e1b04c01ac587f8fda528e8b5
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-04-26 13:14:28 +0100

    update copyright notice

--- src/Exception.php
+++ src/Exception.php
@@ -3,7 +3,7 @@
  * Component for generating, rendering and parsing DOM elements.
  *
  * @package     posterno
- * @copyright   Copyright (c) 2019, Sematico, LTD
+ * @copyright   Copyright (c) 2009-2019, NOLA Interactive, LLC.
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       0.1.0
  */
